14667cd6e67b019d29e770a58d5da155dd8e4c64
josue <josuearrieche77@gmail.com>
2021-08-04 17:40:29 -0500

    bug f1x ajuste deploy

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -25,9 +25,7 @@ sap.ui.define([
 							fnResolve(oData);
 						},
 						error: function (error, status, err) {
-							if(err = 500){
-								modelo = 
-							}
+							
 							//sap.ui.core.BusyIndicator.hide();
 							fnReject(new Error(error.message));
 						}
@@ -39,7 +37,7 @@ sap.ui.define([
 		 * @public
 		 * @returns {sap.ui.core.routing.Router} the router for this component
 		 */
-		getRouter : function () {
+		getRouter: function () {
 			return UIComponent.getRouterFor(this);
 		},
 
@@ -49,7 +47,7 @@ sap.ui.define([
 		 * @param {string} [sName] the model name
 		 * @returns {sap.ui.model.Model} the model instance
 		 */
-		getModel : function (sName) {
+		getModel: function (sName) {
 			return this.getView().getModel(sName);
 		},
 
@@ -60,7 +58,7 @@ sap.ui.define([
 		 * @param {string} sName the model name
 		 * @returns {sap.ui.mvc.View} the view instance
 		 */
-		setModel : function (oModel, sName) {
+		setModel: function (oModel, sName) {
 			return this.getView().setModel(oModel, sName);
 		},
 
@@ -69,7 +67,7 @@ sap.ui.define([
 		 * @public
 		 * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
 		 */
-		getResourceBundle : function () {
+		getResourceBundle: function () {
 			return this.getOwnerComponent().getModel("i18n").getResourceBundle();
 		},
 
@@ -77,13 +75,14 @@ sap.ui.define([
 		 * Event handler when the share by E-Mail button has been clicked
 		 * @public
 		 */
-		onShareEmailPress : function () {
+		onShareEmailPress: function () {
 			var oViewModel = (this.getModel("objectView") || this.getModel("worklistView"));
 			URLHelper.triggerEmail(
 				null,
 				oViewModel.getProperty("/shareSendEmailSubject"),
 				oViewModel.getProperty("/shareSendEmailMessage")
 			);
-		}	});
+		}
+	});
 
 });
